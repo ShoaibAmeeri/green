181be11bbe188857c593a3b85765148567c44a1a
Shoaib Ameeri <140954952+ShoaibAmeeri@users.noreply.github.com>
2024-01-29 20:35:05 +0500

    loader added

--- src/pages/SignIn/Signup.jsx
+++ src/pages/SignIn/Signup.jsx
@@ -1,42 +1,42 @@
 import { useFormik } from "formik";
 import { Link } from "react-router-dom";
 import { signUpSchema } from "../../schema";
-import postData  from "../../api/api" ;
+import postData from "../../api/api";
+import { useState } from "react";
+
 
 const initialValues = {
   name: "",
   email: "",
   password: "",
   confirm_password: "",
- 
 };
 
-
 export default function Signup() {
+
+  const [loading, setloading] = useState(false)
+
   const { values, errors, handleBlur, touched, handleChange, handleSubmit } =
     useFormik({
       initialValues: initialValues,
       validationSchema: signUpSchema,
       onSubmit: (values) => {
         console.log(values);
-        postData("http://localhost:8000/users", values).then((data) => {
-          console.log(data); // JSON data parsed by `data.json()` call
-        }).catch((error)=>{
-          alert("heres some exception")
-          console.log(error);
-        })
-      }
+        setloading(true)
+        postData("http://localhost:8000/users", values)
+          .then((data) => {
+            console.log(data);
+            setloading(false)
+          })
+          .catch((error) => {
+            alert("heres some exception");
+            console.log(error);
+            setloading(false)
+          });
+      },
     });
-  
-
-
 
   return (
-    //   <div className="w-full bg-white mx-auto dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-    //       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-
-    //       </div>
-    //   </div>
     <>
       <div className=" sm:mx-auto sm:w-full sm:max-w-sm bg-white py-5 px-10 rounded-xl shadow-lg">
         <div className="flex items-center justify-between  md:block">
@@ -91,7 +91,7 @@ export default function Signup() {
               onChange={handleChange}
               onBlur={handleBlur}
             />
-             {errors.email && touched.email ? (
+            {errors.email && touched.email ? (
               <p className="form-error text-pink-600 font-medium">
                 {" "}
                 {errors.email}{" "}
@@ -114,7 +114,7 @@ export default function Signup() {
               onChange={handleChange}
               onBlur={handleBlur}
             />
-             {errors.password && touched.password ? (
+            {errors.password && touched.password ? (
               <p className="form-error text-pink-600 font-medium">
                 {" "}
                 {errors.password}{" "}
@@ -154,10 +154,9 @@ export default function Signup() {
                 required=""
                 name="terms"
                 value={values.terms}
-              onChange={handleChange}
-              onBlur={handleBlur}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
-              
             </div>
             <div className="ml-3 text-sm">
               <label
@@ -173,19 +172,60 @@ export default function Signup() {
                 </a>
               </label>
               {errors.terms && touched.terms ? (
-              <p className="form-error text-pink-600 font-medium">
-                {" "}
-                {errors.terms }{" "}
-              </p>
-            ) : null}
+                <p className="form-error text-pink-600 font-medium">
+                  {" "}
+                  {errors.terms}{" "}
+                </p>
+              ) : null}
             </div>
           </div>
-          <button
+          {/* <button
             type="submit"
             className="w-full text-white bg-green-500 hover:bg-greenz-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
-            Create an account
+            
+            
+<div role="status" className="">
+    <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Create an account</span>
+</div>
+
+          </button> */}
+
+          <button
+            
+            type="submit"
+            class="text-white w-full text-center justify-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5   me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+          >
+            {
+              loading ? (
+                <>
+                <svg
+                aria-hidden="true"
+                role="status"
+                class="inline w-4 h-4 me-3 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+              Loading...
+              </>) : "create an account"
+            }
+           
           </button>
+
           <p className="text-sm font-light text-gray-500 dark:text-gray-400">
             Already have an account?{" "}
             <Link
--- src/pages/home/Chapters.jsx
+++ src/pages/home/Chapters.jsx
@@ -1,43 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import images from '../../components/data/chapters'
-import ChCard from '../../components/ChCard'
-import axios from 'axios';
-
-
+import React, { useEffect, useState } from "react";
+import images from "../../components/data/chapters";
+import ChCard from "../../components/ChCard";
+import axios from "axios";
 
 // import {axios} from axios
 
 function Chapters() {
-
-  
-  
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get('http://localhost:8000/books')
-    .then(res => setData(res.data))
-    .catch(err => console.log(err))
-    }, [])
-  
-
+    axios
+      .get("http://localhost:8000/books")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className=" mx-auto text-center">
-    <h1 className="my-5 font-bold text-5xl text-orange-500">
-      {" "}
-      <span className="text-green-500">GREEN</span> BOOK CHAPTERS
-    </h1>
-    <div className="grid md:grid-cols-3 sm:grid-cols-2">
-      
-      {data.map((img) => (
-        <>
-        {/* {console.log(img._id)} */}
-        <ChCard src={img.image} key={img._id} id={img._id} />
-        
-        </>
-      ))}
+      <h1 className="my-5 font-bold text-5xl text-orange-500">
+        {" "}
+        <span className="text-green-500">GREEN</span> BOOK CHAPTERS
+      </h1>
+      <div className="grid md:grid-cols-3 sm:grid-cols-2">
+        {data.map((img) => (
+          <>
+            {/* {console.log(img._id)} */}
+            <ChCard src={img.image} key={img._id} id={img._id} />
+          </>
+        ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Chapters
+export default Chapters;
