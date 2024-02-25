882a23396e02eef5b6b90be06345eb7de5633225
Shoaib Ameeri <140954952+ShoaibAmeeri@users.noreply.github.com>
2024-02-25 11:55:48 +0500

    news posted

--- src/pages/home/NewsPaper.jsx
+++ src/pages/home/NewsPaper.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-// import news from '../../components/data/news'
+import React, { useEffect, useState } from 'react'
+
 import NewsCard from '../../components/NewsCard'
+import axios from 'axios';
 function NewsPaper() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/news")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div className="mx-auto text-center ">
     <h1 className="my-5 font-bold text-4xl text-orange-500">
@@ -10,9 +18,9 @@ function NewsPaper() {
     </h1>
 
     <div className="imagesCard grid md:grid-cols-3 sm:grid-cols-2 gap-5 mx-auto ">
-      {/* {news.map((img, i) => (
-        <NewsCard src={img.src} key={i} />
-      ))} */}
+      {data.map((img, i) => (
+        <NewsCard src={img.image} key={i} />
+      ))}
     </div>
   </div>
   )
